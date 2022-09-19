8fc8f32235f3732ba7e7e53bdfbc1096badb5f2a
Daria <dar.zavaliuk@gmail.com>
2022-09-19 23:15:33 +0300

    feat: add functions for searching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import { Users } from "./users";
+import React, { useState } from "react";
+import Table from "./Table";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [query, setQuery] = useState("");
+
+    const keys = ["first_name", "last_name", "email"];
+    const search = (data) => {
+        return data.filter((item) =>
+            keys.some((key) => item[key].toLowerCase().includes(query))
+        );
+    };
+
+    return (
+        <div className="app">
+            <input
+                type="text"
+                placeholder="Search..."
+                className="search"
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            {/* <ul className="list">
+                {Users.filter((user) =>
+                    user.first_name.toLowerCase().includes(query)
+                ).map((user) => (
+                    <li key={user.id} className="listItem">
+                        {user.first_name}
+                    </li>
+                ))}
+            </ul> */}
+            <Table data={search(Users)} />
+        </div>
+    );
 }
 
 export default App;
